57a4a46362e83cd0b1d60205232fc81b3151113d
waffle52 <asuka9767@protonmail.com>
2021-12-06 14:57:08 -0800

    Changed back as was in use by apache

--- main.py
+++ main.py
@@ -26,4 +26,4 @@ app.include_router(api_router)
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=80, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
