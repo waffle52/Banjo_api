8222216900017489f74306406679a2d98c85f899
waffle52 <asuka9767@protonmail.com>
2021-12-06 19:06:40 -0800

    Changed back to 8001; Was not home earlier to change port settings for testing

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ app.include_router(api_router)
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=443, log_level="debug")
+    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
