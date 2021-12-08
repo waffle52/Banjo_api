099459d6dc7618a9a3de4b8b50b4f48e7171d017
waffle52 <asuka9767@protonmail.com>
2021-12-08 14:57:57 -0800

    Changed app name to actual app

--- main.py
+++ main.py
@@ -2,14 +2,14 @@
 # this script starts an API application using 
 from fastapi import FastAPI, APIRouter
 
-app = FastAPI()
+Banjo = FastAPI()
 
 
-@app.get("/")
+@Banjo.get("/")
 async def root():
     return {"message": "Hello World"}
 
 if __name__ == "__main__":
     # Use this for debugging purposes only
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8001, log_level="debug")
+    uvicorn.run(Banjo, host="0.0.0.0", port=8001, log_level="debug")
