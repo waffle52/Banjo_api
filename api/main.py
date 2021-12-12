fe8305cc8e7105555b2f8077c24b5a9cc4757f11
waffle52 <asuka9767@protonmail.com>
2021-12-11 16:10:22 -0800

    Moved main api file to api folder; Added folders for future parts of api; Changed main code for feature in development; Added Authors file;

---
+++ api/main.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+# This script starts an API application using FastAPI to manage my server
+from fastapi import FastAPI, APIRouter
+fom models import *
+
+Banjo = FastAPI()
+
+
+# PacManScoreTracker HTTP Methods
+@Banjo.get("/PacManScoreTracker/{score}")
+async def take_score(score):
+    return {"item": score}
+
+
+# practice section.
+# SpaceGhostQuotes HTTP Methods
+@Banjo.get("/SpaceGhostQuotes/")
+async def random_quote():
+    # Gets a random quote from the MySQL database
+    return ("test")
+
+
+@Banjo.get("/SpaceGhostQuotes/{quote_id}")
+async def select_quote(quote_id):
+    # Import from models list of quotes and return quote from index via id
+    return (quote_id)
+
+
+if __name__ == "__main__":
+    # Use this for debugging purposes only
+    import uvicorn
+    uvicorn.run(Banjo, host="0.0.0.0", port=8001, log_level="debug")
---
+++ console.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+# Console application to control the Banjo API
+import cmd
+from datetime import datetime
+import models
+
+
+class BanjoCommand(cmd.Cmd):
+    """ Banjo Console """
+    prompt = "(Banjo) "
+
+    def do_EOF(self, arg):
+        """Exits console"""
+        return True
+
+    def emptyline(self):
+        """ overwriting the emptyline method """
+        return False
+
+    def do_quit(self, arg):
+        """Quit command to exit the program"""
+        return True
