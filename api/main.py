3a1843236daafc2a5d1c0347dd69153d0059835a
waffle52 <asuka9767@protonmail.com>
2021-12-20 13:22:47 -0800

    Added private endpoint

--- api/main.py
+++ api/main.py
@@ -2,10 +2,15 @@
 """main.py
 This script starts an API application using FastAPI to manage my server
 """
-from fastapi import FastAPI, APIRouter
+from fastapi import FastAPI, APIRouter, Depends
+from fastapi.security import HTTPBearer
+
+# Scheme for the Authorization header
+token_auth_scheme = HTTPBearer()
 
 # Create app instance
 Banjo = FastAPI()
+
 
 @Banjo.get("/api/public")
 def public():
@@ -16,7 +21,15 @@ def public():
         "msg": ("Hello from a public endpoint! You don't need to be "
                 "authenticated to see this.")
     }
-    return result
+    return (result)
+
+
+@Banjo.get("/api/private")
+def private(token: str = Depends(token_auth_scheme)):
+    """A valid access token is required to access this route"""
+    result = token.credentials
+    return (result)
+
 
 if __name__ == "__main__":
     # Use this for debugging purposes only
