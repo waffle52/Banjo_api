30acef4f885ef8c69b8e09111d1262f0ecf53372
waffle52 <asuka9767@protonmail.com>
2021-12-20 16:22:07 -0800

    New file for handling config for JWT

---
+++ api/utils.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+"""utils.py
+This file...
+"""
+import os
+import jwt
+from configparser import ConfigParser
+
+
+def set_up():
+     """Sets up configuration for the app"""
+     env = os.getenv("ENV",".config")
+
+     if env == ".config":
+         config = ConfigParser()
+         config.read(".config")
+         config = config["AUTH0"]
+     else:
+         config = {
+             "DOMAIN": os.getenv("DOMAIN", "your.domain.com"),
+             "API_AUDIENCE": os.getenv("API_AUDIENCE", "your.audience.com"),
+             "ISSUER": os.getenv("ISSUER", "https://your.domain.com/"),
+             "ALGORITHMS": os.getenv("ALGORITHMS", "RS256"),
+         }
+    return config
