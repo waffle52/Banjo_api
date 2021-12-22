edc938ea4c5b0009ec5adfece42ccc3c6548c5da
waffle52 <asuka9767@protonmail.com>
2021-12-22 11:20:47 -0800

    Deleted utils file and restarting from scratch to work without auth0

--- api/main.py
+++ api/main.py
@@ -2,43 +2,14 @@
 """main.py
 This script starts an API application using FastAPI to manage my server
 """
-from fastapi import FastAPI, Depends, Response, status
-from fastapi.security import HTTPBearer
-
-from .utils import VerifyToken
-
-# Scheme for the Authorization header
-token_auth_scheme = HTTPBearer()
+from fastapi import FastAPI, Depends
+from fastapi.security import OAuth2PasswordBearer
 
 # Create app instance
 Banjo = FastAPI()
 
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
-@Banjo.get("/api/public")
-def public():
-    """No access token required to access this route"""
-
-    result = {
-        "status": "success",
-        "msg": ("Hello from a public endpoint! You don't need to be "
-                "authenticated to see this.")
-    }
-    return (result)
-
-
-@Banjo.get("/api/private")
-def private(token: str = Depends(token_auth_scheme)):
-    """A valid access token is required to access this route"""
-    result = VerifyToken(token.credentials).verify()
-    if result.get("status"):
-        response.status_code = status.HTTP_400_BAD_REQUEST
-        print("Test: {}".format(result))
-        return (result)
-    print("Test: {}".format(result))
-    return (result)
-
-
-if __name__ == "__main__":
-    # Use this for debugging purposes only
-    import uvicorn
-    uvicorn.run(Banjo, host="0.0.0.0", port=8001, log_level="debug")
+@Banjo.get("/items/")
+async def read_items(token: str = Depends(oauth2_scheme)):
+    return {"token": token}
