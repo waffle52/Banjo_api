6fd214e4b9e64ecf984611ba7a1078df4db65d3e
waffle52 <asuka9767@protonmail.com>
2022-01-07 11:32:01 -0800

    Added call to class for each quote

--- api/models/temp_quotes.py
+++ api/models/temp_quotes.py
@@ -12,6 +12,6 @@ class quote:
 
 quotes = []
 quotes.append(quote("Space Ghost", 0, "Bears are Crazy. They'll bite your head if you're wearing a steak on it."))
-quotes.append("Space Ghost", 1, "Welcome back, stupid viewers! You'll watch anything! Go ahead, change the channel. You'll be back!")
-quotes.append("Space Ghost", 2, "I have a giant brain that is able to reduce any complex machine into a simple yes or no answer.")
-quotes.append("Space Ghost", 3, "Chambraigne: It's shampoo for your hair, and your brain.")
+quotes.append(quote("Space Ghost", 1, "Welcome back, stupid viewers! You'll watch anything! Go ahead, change the channel. You'll be back!"))
+quotes.append(quote("Space Ghost", 2, "I have a giant brain that is able to reduce any complex machine into a simple yes or no answer."))
+quotes.append(quote("Space Ghost", 3, "Chambraigne: It's shampoo for your hair, and your brain."))
