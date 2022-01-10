fc8d380486ca080de4639eafa68f98f4ee2cdcf5
waffle52 <asuka9767@protonmail.com>
2022-01-10 02:55:44 +0000

    Updated console to kill uvicorn app running on server

--- console.py
+++ console.py
@@ -3,6 +3,11 @@
 
 import cmd
 from datetime import datetime
+import psutil
+import os
+import signal
+
+
 
 class BanjoCommand(cmd.Cmd):
     """ Banjo Console """
@@ -12,13 +17,35 @@ class BanjoCommand(cmd.Cmd):
         """Exits console"""
         return True
 
+    def do_test_mode(self, arg=None):
+        """Enable/Disable Stable API for testing """
+        process_name = "uvicorn"
+        pid = None
+        if (arg is None or len(arg) < 1):
+            print("Enable/Disable Stable API")
+            return (False)
+        if (arg.split()[0] == "Enable"):
+            for proc in psutil.process_iter():
+                if process_name in proc.name():
+                    pid = proc.pid
+            if (pid is None):
+                print("Stable API is not Enabled!")
+                return (False)
+            os.kill(pid, signal.SIGTERM)
+        elif (arg.split()[0] == "Disable"):
+            print("Test: Restart job /stable api via cron")
+        else:
+            print("Unknown passed argument")
+        return (False)
+
     def emptyline(self):
-        """ overwriting the emptyline method """
-        return False
+        """Overwriting the emptyline method """
+        return (False)
 
     def do_quit(self, arg):
         """Quit command to exit the program"""
-        return True
+        return (True)
+
 
 if __name__ == '__main__':
     """ Main method """
