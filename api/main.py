ec41cdb27f54c0f1c068e222d37c93022e4a67ed
waffle52 <asuka9767@protonmail.com>
2022-01-31 15:03:13 -0800

    Added import for mysql framework

--- api/main.py
+++ api/main.py
@@ -9,6 +9,7 @@ from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
 import json
+import mysql.connector
 from passlib.context import CryptContext
 from pydantic import BaseModel
 import random
