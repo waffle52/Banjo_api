7bd3e125fa63699c72c0241a2306c7ba207dd94a
waffle52 <asuka9767@protonmail.com>
2022-01-31 15:02:19 -0800

    Added json import

--- api/main.py
+++ api/main.py
@@ -8,6 +8,7 @@ from dotenv import load_dotenv
 from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 from jose import JWTError, jwt
+import json
 from passlib.context import CryptContext
 from pydantic import BaseModel
 import random
