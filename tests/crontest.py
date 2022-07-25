58f6e6b583a35eed70bdf9b058d0fe31b86786b9
waffle52 <asuka9767@protonmail.com>
2022-07-25 16:32:15 -0700

    updated code to check api via ping, prompts user to disable/enable cron job for api

--- tests/crontest.py
+++ tests/crontest.py
@@ -3,20 +3,17 @@
 test script to stop cron jobs and switch features.
 """
 from crontab import CronTab
+import os
 
 # prompt to check if stable/beta is running. it answers.
 # option to enable/re-enable Stable from directory.
 # option to disable stable
 
-# STEP 1 check if stable is running via. cron? bash?
-
-
-# STEP 2 if step 1 shows it is online. Ask to kill it & remove from file? 
-
-# STEP 2.1 if step 1 show it is offline. Ask to enable it by adding it to cron file. 
-
-
 # TEST CODE BELOW
+
+hostname = "www.asukasoftware.com/api/Online"
+response = os.system("ping -c 1 " + hostname)
+
 
 cron = CronTab(user='ubuntu')
 
@@ -24,19 +21,24 @@ job = cron.new(command='cd /home/ubuntu/stable/Banjo_api && /usr/bin/bash /home/
 
 search = cron.find_comment('Stable API')
 
-for item in search:
-    print (item)
+# STEP 1 check if stable is running via. cron? bash? - in progress
 
-# second test with adding job
-job = cron.new(command='cd /home/ubuntu/stable/Banjo_api && /usr/bin/bash /home/ubuntu/stable/Banjo_api/startup.sh', comment='Stable API Running')
-job.minute.every(1)
-
-cron.write()
-
-search = cron.find_comment('Stable API')
-
-for item in search:
-    print (item)
-
-cron.remove(job)
-print ("Job removed")
+if (response == 0):
+    print("Stable API is running!")
+    # STEP 2 if step 1 shows it is online. Ask to kill it & remove from file.
+    val = input("Disable Stable API?: Y/y")
+    if (val == "Y" or val == "y"):
+        cron.remove(job)
+        print ("Job deleted")
+    else:
+        exit()
+else:
+    # step 2.1 if step 1 show it is offline. Ask to run it and add to file.
+    print("Stable API is not running")
+    val = input("Enable Stable API?: Y/y")
+    if (val == "Y" or val == "y"):
+        job.minute.every(1) # check later for proper scheduling
+        cron.write()
+        print ("Job created")
+    else:
+        exit()
